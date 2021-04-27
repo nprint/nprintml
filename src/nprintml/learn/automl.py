523e100f62961f2d5d55e984e70667ef6ca2d951
JordanHolland <jordanah@princeton.edu>
2021-04-27 10:21:37 -0400

    known features in and remove feature generators

--- src/nprintml/learn/automl.py
+++ src/nprintml/learn/automl.py
@@ -13,6 +13,8 @@ import itertools
 import matplotlib.pyplot as plt
 import seaborn as sns
 from autogluon.tabular import TabularPredictor
+from autogluon.features.generators import AutoMLPipelineFeatureGenerator
+from autogluon.core.features.feature_metadata import FeatureMetadata
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import (
@@ -93,6 +95,16 @@ class AutoML:
         """Train prospective models."""
         # predictor gives us default access to the *best* predictor that
         # was trained on the task (otherwise we're just wrapping AutoGluon)
+        fg = AutoMLPipelineFeatureGenerator(enable_categorical_features=False,
+                                            enable_datetime_features=False,
+                                            enable_text_special_features=False,
+                                            enable_text_ngram_features=False
+                                           )
+        d = {}
+        for col in train_data.columns[:-1]:
+            d[col] = 'int'
+        fmd = FeatureMetadata(d)
+
         task = TabularPredictor(
             label='label',
             eval_metric=eval_metric,
@@ -103,6 +115,8 @@ class AutoML:
             train_data=train_data,
             time_limit=time_limit,
             presets=self.QUALITY_PRESETS[quality],
+            feature_generator=fg,
+            feature_metadata=fmd
         )
 
     def test(self, predictor, test_data):

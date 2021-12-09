413369b2c9dbb77ad2e241327135a15e6e1ac9c6
undefined <jesselondon@gmail.com>
2021-12-09 22:30:10 +0000

    Bump version: 1.0.4 → 1.1.0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ EXTRAS_REQUIRE = {
 
 
 setup(name='nprintml',
-      version='1.0.4',
+      version='1.1.0',
       description='Fully automated traffic analysis with nPrint',
       long_description=README_PATH.read_text(),
       long_description_content_type="text/markdown",

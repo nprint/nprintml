f0d31a358af181442624f986c521b52309232b11
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-12 23:21:35 +0000

    Adding tarfile member sanitization to extractall()

--- src/nprintml/net/install.py
+++ src/nprintml/net/install.py
@@ -267,7 +267,26 @@ def execute_bootstrap(commands):
         urllib.request.urlretrieve(NPRINT_ARCHIVE_URL, archive_path)
 
         with tarfile.open(archive_path) as release_archive:
-            release_archive.extractall(tempdir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(release_archive, tempdir)
 
         for command in commands:
             print('→', *command, end='\n\n')
